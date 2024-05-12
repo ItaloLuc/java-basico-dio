006de7feb0a2152f8e1adaca56a2717915843b91
ItaloLuc <167926719+ItaloLuc@users.noreply.github.com>
2024-05-12 14:44:21 -0300

    Fim da primeira semana

--- trilha-java-basico/trilha-java-basico/src/edu/italo/primeirasemana/tiposvariaveis/TiposVariaveis.java
+++ trilha-java-basico/trilha-java-basico/src/edu/italo/primeirasemana/tiposvariaveis/TiposVariaveis.java
@@ -2,6 +2,28 @@ package edu.italo.primeirasemana.tiposvariaveis;
 
 public class TiposVariaveis {
     
+    public static void main(String [] args) throws Exception{
 
+    //Exemplos tipos de variaveis
+        byte idade = 123;
+        short ano = 2024;
+        int cep = 21070333; //caso comece com zero, talvez seja necessário trocar o tipo
+        long cpf = 12312312312L; //caso comece com zero, talvez seja necessário trocar o tipo
+        float pi = 3.14F;
+        double salario = 1245.33;
+
+
+    //Exemplo do quão tipado Java é
+        short numeroCurto = 1;
+        int numeroNormal1 = numeroCurto;
+        short numeroCurto2 = numeroNormal1; //Possível solução: short = numeroNormal1;
+
+    //Constantes
+        final double SECULO = 2.1; //Por convenção, constantes serão escritas em CAPSLOCK
+
+        SECULO = 22; //Como desclarei que "seculo" seria uma constante (utilizando a palavra reservada "final"), o java não me permite alterar seu valor posteriormente
+
+
+    }
     
 }
