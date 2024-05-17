ccd50145d815da8c2de9060dfe69938bf86108ff
ItaloLuc <167926719+ItaloLuc@users.noreply.github.com>
2024-05-17 10:54:21 -0300

    Corrigida a indentação

--- trilha-java-basico/trilha-java-basico/src/edu/italo/segundasemana/Escopo/Escopo.java
+++ trilha-java-basico/trilha-java-basico/src/edu/italo/segundasemana/Escopo/Escopo.java
@@ -20,17 +20,18 @@ public class Escopo {
         //variavel da classe conta
         double saldo=10.0;
         
-        public void sacar(Double valor) {
+    public void sacar(Double valor) {
             //variavel local de método
             double novoSaldo = saldo - valor;
         }
-}   public void imprimirSaldo(){
-    //disponível em toda classe
-    System.out.println(saldo);
-    //somente o método sacar conhece esta variavel
-    System.out.println(novoSaldo);
+    }   
+    public void imprimirSaldo(){
+        //disponível em toda classe
+        System.out.println(saldo);
+        //somente o método sacar conhece esta variavel
+        System.out.println(novoSaldo);
 
-}
+    }
     public double calcularDividaExponencial(){
 		//variável local de método
 		double valorParcela = 50.0;
