ee3c659fa4a2ca346b1ab008b6b7301b693f1b20
Ítalo Lúcio <167926719+ItaloLuc@users.noreply.github.com>
2024-06-16 12:10:51 -0300

    Feat: Refatora CalculadoraDeMedias para calcular e exibir a média da turma com números decimais
    - Altera o tipo de retorno do método calculaMediaDaTurma para double para calcular a média com precisão decimal.
    - Atualiza a variável media na função main para double para armazenar a média calculada corretamente.
    - Ajusta a formatação da saída no printf para exibir a média da turma com precisão decimal.

--- trilha-java-basico/trilha-java-basico/src/edu/italo/terceirasemana/Debbuging/CalculadoraDeMedias.java
+++ trilha-java-basico/trilha-java-basico/src/edu/italo/terceirasemana/Debbuging/CalculadoraDeMedias.java
@@ -1,5 +1,28 @@
-package terceirasemana.Debbuging;
+package edu.italo.terceirasemana.Debbuging;
+
+import java.util.Scanner;
 
 public class CalculadoraDeMedias {
-    
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String[] alunos = {"Italo", "Carla", "Pedro"};
+
+        double media = calculaMediaDaTurma(alunos, scan);
+
+        System.out.printf("Média da turma %f", media);
+    }
+
+    public static double calculaMediaDaTurma(String[] alunos, Scanner scanner){
+
+        int soma = 0;
+        for(String aluno : alunos){
+            System.out.printf("Nota do aluno %s: ", aluno);
+            int nota = scanner.nextInt();
+            soma += nota;
+        }
+
+        return (double) soma / alunos.length;
+    }
+
 }
+
